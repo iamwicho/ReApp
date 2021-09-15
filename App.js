8bd8eb5bcf2bfc65197179e54cfe3b9cfff4d41e
Luis Andrade Macias <luisenriqueandrademacias@gmail.com>
2021-09-15 07:31:54 -0500

    Version del proyecto que se vio el 15 de septiembre del 2021

--- App.js
+++ App.js
@@ -7,6 +7,9 @@ import {
   Center,
   Heading,
   Container,
+  Stack,
+  VStack,
+  Button
 } from "native-base";
 
 export default function App() {
@@ -27,6 +30,10 @@ export default function App() {
     },
   });
 
+  const Example = () => {
+    return <Button onPress={() => console.log("hola mundo")}>PRIMARY</Button> 
+  }
+
   return (
     <NativeBaseProvider theme={theme}>
       <Box
@@ -39,14 +46,18 @@ export default function App() {
       </Box>
       <Center flex={8}>
         <Container>
-          <Heading>
-            A component library for the
-            <Heading color="emerald.400"> React Ecosystem</Heading>
+          <Heading> A component library for the <Heading color="emerald.400"> React Ecosystem</Heading>
           </Heading>
-          <Heading p={4} fontSize="md" fontWeight="hairline">
-            NativeBase is a simple, modular and accessible component library
+          <Heading p={4} fontSize="md" fontWeight="hairline"> NativeBase is a simple, modular and accessible component library
             that gives you building blocks to build you React applications.
           </Heading>
+          <VStack alignItems="center" space={3}>
+              <Center bg="primary.400" size={16} _text={{color:"white"}}> Text </Center>
+              <Center bg="primary.200" size={16} > Text </Center>
+              <Center bg="primary.500" size={16} > Text </Center>
+          </VStack>
+          <Example></Example>
+
         </Container>
       </Center>
     </NativeBaseProvider>
